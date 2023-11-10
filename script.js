2d1f81bbec0e9c7cb0aca84f0c135e64a4f3e0fd
Frangzzsosa <A01285092@tec.mx>
2023-11-10 16:07:34 -0600

    agregue que el primer clima que te aparece es el del lugar donde te encuentras, tambien aguegrue que solo aparezca el clima de hoy y le tengas que picar a un boton para que te muestre los demas dias

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiKey = '163b59841a67eadb0868aff7a9887d25';
+let expanded = false; // Variable para rastrear si el pronóstico está expandido
 
 function buscarPronostico() {
     const cityInput = document.getElementById('city');
@@ -7,12 +8,62 @@ function buscarPronostico() {
 
     const city = cityInput.value;
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; // units=metric para obtener temperaturas en Celsius
+    // Obtener la ubicación actual del usuario
+    if (!city && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            // Realizar una solicitud a la API de OpenWeatherMap con las coordenadas actuales
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const city = data.name;
+                    const country = data.sys.country;
+                    actualizarTitulo(`${city}, ${country}`);
+                    mostrarPronostico(city);
+                })
+                .catch(error => {
+                    console.error('Error al obtener el pronóstico del tiempo: ', error);
+                });
+        }, error => {
+            console.error('Error al obtener la ubicación: ', error);
+        });
+    } else {
+        // Si se ingresa una ciudad, realizar la búsqueda normal
+        obtenerPaisPorCiudad(city);
+    }
+}
+
+function obtenerPaisPorCiudad(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            cityName.textContent = data.city.name;
+            const country = data.sys.country;
+            actualizarTitulo(`${city}, ${country}`);
+            mostrarPronostico(city);
+        })
+        .catch(error => {
+            console.error('Error al obtener el pronóstico del tiempo: ', error);
+        });
+}
+
+function actualizarTitulo(title) {
+    const cityName = document.getElementById('city-name');
+    cityName.textContent = `Pronóstico para ${title}`;
+}
+
+function mostrarPronostico(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const forecastDiv = document.getElementById('forecast');
             forecastDiv.innerHTML = '';
 
             const dailyTemperatures = {};
@@ -37,12 +88,32 @@ function buscarPronostico() {
                 }
             });
 
-            for (const day in dailyTemperatures) {
-                const { maxTemp, minTemp } = dailyTemperatures[day];
+            const today = new Date().toLocaleDateString('es-ES', { weekday: 'long' }).charAt(0).toUpperCase() + new Date().toLocaleDateString('es-ES', { weekday: 'long' }).slice(1);
+            const { maxTemp, minTemp } = dailyTemperatures[today];
 
-                const forecastItem = document.createElement('div');
-                forecastItem.innerHTML = `<p>${day} - Máx: ${maxTemp.toFixed(2)}°C / Mín: ${minTemp.toFixed(2)}°C</p>`;
-                forecastDiv.appendChild(forecastItem);
+            const forecastItem = document.createElement('div');
+            forecastItem.innerHTML = `<p>${today} - Máx: ${maxTemp.toFixed(2)}°C / Mín: ${minTemp.toFixed(2)}°C</p>`;
+            forecastDiv.appendChild(forecastItem);
+
+            // Agregar la flecha para expandir/cerrar el pronóstico
+            const toggleButton = document.getElementById('toggle-button');
+            if (Object.keys(dailyTemperatures).length > 1) {
+                toggleButton.style.display = 'block';
+                if (expanded) {
+                    for (const day in dailyTemperatures) {
+                        if (day !== today) {
+                            const { maxTemp, minTemp } = dailyTemperatures[day];
+                            const forecastItem = document.createElement('div');
+                            forecastItem.innerHTML = `<p>${day} - Máx: ${maxTemp.toFixed(2)}°C / Mín: ${minTemp.toFixed(2)}°C</p>`;
+                            forecastDiv.appendChild(forecastItem);
+                        }
+                    }
+                    toggleButton.textContent = 'Ocultar Pronóstico';
+                } else {
+                    toggleButton.textContent = 'Mostrar Pronóstico';
+                }
+            } else {
+                toggleButton.style.display = 'none';
             }
         })
         .catch(error => {
@@ -50,5 +121,12 @@ function buscarPronostico() {
         });
 }
 
+function toggleForecast() {
+    expanded = !expanded;
+    buscarPronostico();
+}
+
+// Llamar a buscarPronostico al cargar la página para obtener la ubicación actual
+buscarPronostico();
 
 
