c51a15921253c9c51b0c6c5915c9a01e02d5f696
Frangzzsosa <A01285092@tec.mx>
2023-11-13 16:24:45 -0600

    Un diseño nuevo y funciones nuevas

--- script.js
+++ script.js
@@ -1,132 +1,104 @@
-const apiKey = '163b59841a67eadb0868aff7a9887d25';
-let expanded = false; // Variable para rastrear si el pronóstico está expandido
+const apiKey = "163b59841a67eadb0868aff7a9887d25";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-function buscarPronostico() {
-    const cityInput = document.getElementById('city');
-    const cityName = document.getElementById('city-name');
-    const forecastDiv = document.getElementById('forecast');
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-    const city = cityInput.value;
+// Función para obtener la información del clima basada en la ciudad
+async function getWeatherByCity(city) {
+    try {
+        const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error(`Error al buscar la ciudad: ${response.statusText}`);
+        }
 
-    // Obtener la ubicación actual del usuario
-    if (!city && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-
-            // Realizar una solicitud a la API de OpenWeatherMap con las coordenadas actuales
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const city = data.name;
-                    const country = data.sys.country;
-                    actualizarTitulo(`${city}, ${country}`);
-                    mostrarPronostico(city);
-                })
-                .catch(error => {
-                    console.error('Error al obtener el pronóstico del tiempo: ', error);
-                });
-        }, error => {
-            console.error('Error al obtener la ubicación: ', error);
-        });
-    } else {
-        // Si se ingresa una ciudad, realizar la búsqueda normal
-        obtenerPaisPorCiudad(city);
+        const data = await response.json();
+        updateWeatherData(data);
+    } catch (error) {
+        console.error(error.message);
+        console.log(response); // Agrega esta línea
     }
 }
 
-function obtenerPaisPorCiudad(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// Función para obtener la ubicación actual y consultar el clima
+function getCurrentLocationWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const country = data.sys.country;
-            actualizarTitulo(`${city}, ${country}`);
-            mostrarPronostico(city);
-        })
-        .catch(error => {
-            console.error('Error al obtener el pronóstico del tiempo: ', error);
-        });
-}
-
-function actualizarTitulo(title) {
-    const cityName = document.getElementById('city-name');
-    cityName.textContent = `Pronóstico para ${title}`;
-}
-
-function mostrarPronostico(city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const forecastDiv = document.getElementById('forecast');
-            forecastDiv.innerHTML = '';
-
-            const dailyTemperatures = {};
-
-            data.list.forEach(item => {
-                const date = new Date(item.dt * 1000);
-                const day = date.toLocaleDateString('es-ES', { weekday: 'long' }).charAt(0).toUpperCase() + date.toLocaleDateString('es-ES', { weekday: 'long' }).slice(1); // Capitalizar la primera letra
-                const temperature = item.main.temp;
-
-                if (!dailyTemperatures[day]) {
-                    dailyTemperatures[day] = {
-                        maxTemp: temperature,
-                        minTemp: temperature
-                    };
-                } else {
-                    if (temperature > dailyTemperatures[day].maxTemp) {
-                        dailyTemperatures[day].maxTemp = temperature;
-                    }
-                    if (temperature < dailyTemperatures[day].minTemp) {
-                        dailyTemperatures[day].minTemp = temperature;
-                    }
+            try {
+                // Hacer la solicitud a la API de OpenWeatherMap con las coordenadas
+                const response = await fetch(`${apiUrl}&lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+                if (!response.ok) {
+                    throw new Error(`Error al obtener la ubicación actual: ${response.statusText}`);
                 }
-            });
 
-            const today = new Date().toLocaleDateString('es-ES', { weekday: 'long' }).charAt(0).toUpperCase() + new Date().toLocaleDateString('es-ES', { weekday: 'long' }).slice(1);
-            const { maxTemp, minTemp } = dailyTemperatures[today];
-
-            const forecastItem = document.createElement('div');
-            forecastItem.innerHTML = `<p>${today} - Máx: ${maxTemp.toFixed(2)}°C / Mín: ${minTemp.toFixed(2)}°C</p>`;
-            forecastDiv.appendChild(forecastItem);
-
-            // Agregar la flecha para expandir/cerrar el pronóstico
-            const toggleButton = document.getElementById('toggle-button');
-            if (Object.keys(dailyTemperatures).length > 1) {
-                toggleButton.style.display = 'block';
-                if (expanded) {
-                    for (const day in dailyTemperatures) {
-                        if (day !== today) {
-                            const { maxTemp, minTemp } = dailyTemperatures[day];
-                            const forecastItem = document.createElement('div');
-                            forecastItem.innerHTML = `<p>${day} - Máx: ${maxTemp.toFixed(2)}°C / Mín: ${minTemp.toFixed(2)}°C</p>`;
-                            forecastDiv.appendChild(forecastItem);
-                        }
-                    }
-                    toggleButton.textContent = 'Ocultar Pronóstico';
-                } else {
-                    toggleButton.textContent = 'Mostrar Pronóstico';
-                }
-            } else {
-                toggleButton.style.display = 'none';
+                const data = await response.json();
+                updateWeatherData(data);
+            } catch (error) {
+                console.error(error.message);
+                console.log(response); // Agrega esta línea
             }
-        })
-        .catch(error => {
-            console.error('Error al obtener el pronóstico del tiempo: ', error);
+        }, (error) => {
+            console.error("Error al obtener la ubicación:", error);
         });
+    } else {
+        console.error("La geolocalización no es compatible en este navegador.");
+    }
 }
 
-function toggleForecast() {
-    expanded = !expanded;
-    buscarPronostico();
+// Función para actualizar la interfaz con la información del clima
+function updateWeatherData(data) {
+    document.querySelector(".city").innerHTML = `${data.name}, ${data.sys.country}`;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    // Actualizar el ícono del clima según el tipo de clima
+    updateWeatherIcon(data.weather[0].main);
+
+    // Mostrar la sección del clima
+    document.querySelector(".weather").style.display = "block";
 }
 
-// Llamar a buscarPronostico al cargar la página para obtener la ubicación actual
-buscarPronostico();
+// Función para actualizar el ícono del clima
+function updateWeatherIcon(weatherType) {
+    switch (weatherType.toLowerCase()) {
+        case "clear":
+            weatherIcon.src = "imagenes/clear.png";
+            break;
+        case "clouds":
+            weatherIcon.src = "imagenes/clouds.png";
+            break;
+        case "rain":
+            weatherIcon.src = "imagenes/rain.png";
+            break;
+        case "drizzle":
+            weatherIcon.src = "imagenes/drizzle.png";
+            break;
+        case "mist":
+            weatherIcon.src = "imagenes/mist.png";
+            break;
+        default:
+            weatherIcon.src = "imagenes/default.png";
+            break;
+    }
+}
+
+// Agregar un event listener al botón de búsqueda
+searchBtn.addEventListener("click", () => {
+    // Llamar a la función getWeatherByCity con el valor del campo de búsqueda
+    const cityName = searchBox.value;
+    if (cityName) {
+        getWeatherByCity(cityName);
+    } else {
+        console.error("Por favor, ingrese el nombre de la ciudad.");
+    }
+});
+
+// Llamar a la función para obtener la ubicación actual y consultar el clima al cargar la página
+document.addEventListener("DOMContentLoaded", getCurrentLocationWeather);
 
 
